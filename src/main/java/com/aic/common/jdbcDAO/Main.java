6570682b63f03e4887ef49254de6cb8309ffc0db
user.arvind <arvindjavadeveloper@gmail.com>
2014-12-26 15:22:03 +0800

    Commit

--- src/main/java/com/aic/common/jdbcDAO/Main.java
+++ src/main/java/com/aic/common/jdbcDAO/Main.java
@@ -1,12 +1,15 @@
 package com.aic.common.jdbcDAO;
 
 import java.sql.Connection;
-import java.sql.SQLException;
-
-import javax.sql.DataSource;
+import java.util.UUID;
 
 import org.springframework.context.ApplicationContext;
 import org.springframework.context.support.ClassPathXmlApplicationContext;
+
+import com.aic.common.dao.jdbc.SimpleJdbcTemplateDao;
+import com.aic.common.dao.jdbc.SimpleNamedParameterJdbcTemplateDao;
+import com.aic.common.dao.jdbc.impl.SimpleJdbcTemplateDaoImpl;
+import com.aic.common.dao.jdbc.impl.SimpleNamedParameterJdbcTemplateDaoImpl;
 
 public class Main {
 
@@ -18,15 +21,29 @@ public class Main {
     }
     
     public Connection showDbCatlog() {
-	DataSource dataSource = applicationContext.getBean(DataSource.class);
-	try {
-	    System.out.println(dataSource.getConnection().getCatalog());
-	    return dataSource.getConnection();
-	} catch (SQLException e) {
-	    e.printStackTrace();
-	    return null;
-	}
+	SimpleNamedParameterJdbcTemplateDao simpleDao = applicationContext.getBean(SimpleNamedParameterJdbcTemplateDaoImpl.class);
 	
+	simpleDao.insert(UUID.randomUUID().toString());
+	
+	simpleDao.delete(1);
+	
+	simpleDao.update(2);
+	
+	//simpleDao.selectAll();
+	
+	simpleDao.selectWithUuid("dd");
+	
+	simpleDao.getUuid(2);
+	
+	simpleDao.showDomainObj(5);
+	
+	//simpleDao.showDomainObj();
+	
+	//simpleDao.executeDdl();
+	
+	simpleDao.executePlSql();
+	
+	return null;
     }
 
     public Main() {
--- src/test/java/com/aic/common/jdbcDAO/MainTest.java
+++ src/test/java/com/aic/common/jdbcDAO/MainTest.java
@@ -1,23 +1,113 @@
 package com.aic.common.jdbcDAO;
 
-import java.util.ArrayList;
-import java.util.List;
+import java.util.UUID;
 
 import org.junit.After;
 import org.junit.AfterClass;
 import org.junit.Assert;
 import org.junit.Before;
 import org.junit.BeforeClass;
+import org.springframework.context.ApplicationContext;
+import org.springframework.context.support.ClassPathXmlApplicationContext;
+
+import com.aic.common.dao.jdbc.SimpleJdbcTemplateDao;
+import com.aic.common.dao.jdbc.SimpleNamedParameterJdbcTemplateDao;
+import com.aic.common.dao.jdbc.impl.SimpleJdbcTemplateDaoImpl;
+import com.aic.common.dao.jdbc.impl.SimpleNamedParameterJdbcTemplateDaoImpl;
 
 public class MainTest {
 
-    private List<String> list;
+    private ApplicationContext applicationContext;
     
     @org.junit.Test
     public void testConnection() {
 	Main obj = new Main();
-	Assert.assertNotNull(obj.showDbCatlog());
+	Assert.assertNull(obj.showDbCatlog());
     }
+    
+    /*@org.junit.Test
+    public void testCase1() {
+	SimpleJdbcTemplateDao simpleDao = applicationContext.getBean(SimpleJdbcTemplateDaoImpl.class);
+	Assert.assertNotEquals(0, simpleDao.insert(UUID.randomUUID().toString()));
+    }
+    
+    @org.junit.Test
+    public void testCase2() {
+	SimpleJdbcTemplateDao simpleDao = applicationContext.getBean(SimpleJdbcTemplateDaoImpl.class);
+	simpleDao.delete(1);
+    }
+    
+    @org.junit.Test
+    public void testCase3() {
+	SimpleJdbcTemplateDao simpleDao = applicationContext.getBean(SimpleJdbcTemplateDaoImpl.class);
+	Assert.assertNotEquals(0, simpleDao.update(2));
+    }
+    
+    @org.junit.Test
+    public void testCase4() {
+	SimpleJdbcTemplateDao simpleDao = applicationContext.getBean(SimpleJdbcTemplateDaoImpl.class);
+	Assert.assertNotEquals(0, simpleDao.selectAll());
+    }
+    
+    @org.junit.Test
+    public void testCase5() {
+	SimpleJdbcTemplateDao simpleDao = applicationContext.getBean(SimpleJdbcTemplateDaoImpl.class);
+	Assert.assertNotEquals(0, simpleDao.selectWithUuid("dd"));
+    }
+    
+    @org.junit.Test
+    public void testCase6() {
+	SimpleJdbcTemplateDao simpleDao = applicationContext.getBean(SimpleJdbcTemplateDaoImpl.class);
+	org.junit.Assert.assertNotNull(simpleDao.getUuid(2));
+    }
+    
+    @org.junit.Test
+    public void testCase7() {
+	SimpleJdbcTemplateDao simpleDao = applicationContext.getBean(SimpleJdbcTemplateDaoImpl.class);
+	org.junit.Assert.assertNotNull(simpleDao.showDomainObj(3));
+    }
+    
+    @org.junit.Test
+    public void testCase8() {
+	SimpleJdbcTemplateDao simpleDao = applicationContext.getBean(SimpleJdbcTemplateDaoImpl.class);
+	org.junit.Assert.assertFalse(simpleDao.showDomainObj().isEmpty());
+    }*/
+    
+    public void testCase1() {
+	SimpleNamedParameterJdbcTemplateDao simpleDao = applicationContext.getBean(SimpleNamedParameterJdbcTemplateDaoImpl.class);
+	Assert.assertNotEquals(0, simpleDao.insert(UUID.randomUUID().toString()));
+    }
+    
+    @org.junit.Test
+    public void testCase2() {
+	SimpleNamedParameterJdbcTemplateDao simpleDao = applicationContext.getBean(SimpleNamedParameterJdbcTemplateDaoImpl.class);
+	simpleDao.delete(1);
+    }
+    
+    @org.junit.Test
+    public void testCase3() {
+	SimpleNamedParameterJdbcTemplateDao simpleDao = applicationContext.getBean(SimpleNamedParameterJdbcTemplateDaoImpl.class);
+	Assert.assertNotEquals(0, simpleDao.update(2));
+    }
+    
+    @org.junit.Test
+    public void testCase5() {
+	SimpleNamedParameterJdbcTemplateDao simpleDao = applicationContext.getBean(SimpleNamedParameterJdbcTemplateDaoImpl.class);
+	Assert.assertNotEquals(0, simpleDao.selectWithUuid("dd"));
+    }
+    
+    @org.junit.Test
+    public void testCase6() {
+	SimpleNamedParameterJdbcTemplateDao simpleDao = applicationContext.getBean(SimpleNamedParameterJdbcTemplateDaoImpl.class);
+	org.junit.Assert.assertNotNull(simpleDao.getUuid(2));
+    }
+    
+    @org.junit.Test
+    public void testCase7() {
+	SimpleNamedParameterJdbcTemplateDao simpleDao = applicationContext.getBean(SimpleNamedParameterJdbcTemplateDaoImpl.class);
+	org.junit.Assert.assertNotNull(simpleDao.showDomainObj(3));
+    }
+    
 
     @BeforeClass
     public static void beforeClass() {
@@ -31,25 +121,12 @@ public class MainTest {
 
     @Before
     public void before() {
-	list = new ArrayList<String>(0);
-	System.out.println("Before");
+	applicationContext = new ClassPathXmlApplicationContext("applicationContext-datasource.xml");
     }
 
     @After
     public void after() {
-	System.out.println("After");
+	applicationContext = new ClassPathXmlApplicationContext("applicationContext-datasource.xml");
     }
-
-   /* @org.junit.Test
-    public void isEmpty() {
-	org.junit.Assert.assertTrue(list.isEmpty());
-	System.out.println("isEmpty");
-    }
-
-    @org.junit.Test
-    public void add() {
-	list.add("Arvind");
-	System.out.println("add");
-    }*/
 
 }
